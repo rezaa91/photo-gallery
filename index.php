51a6766ef62def946be3eebf540127d7055c3b10
Rezaa91 <rezaa91@hotmail.co.uk>
2018-05-08 05:49:10 +0100

    markup changed to include sections: recent uploads, social media, view gallery btn

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require('core/init.php');
 
 //include header, view and footer
-$css = '<link rel="stylesheet" href="src/css/index.css" />';//set individual stylesheet
 $page_title = "Homepage"; //set page title
 include('includes/header.inc.php');
 include('views/index.html');
