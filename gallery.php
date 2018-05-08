ad4fcad5344966414167dd4abd7f3fe09435b252
Rezaa91 <rezaa91@hotmail.co.uk>
2018-05-08 21:59:32 +0100

    view images in database on gallery page

--- gallery.php
+++ gallery.php
@@ -1,11 +1,33 @@
 <?php
 require('core/init.php');
 
-//include header, view and footer
-$page_title = "Gallery"; //set page title
-include('includes/header.inc.php');
-include('views/gallery.html');
-include('includes/footer.inc.php');
+
+try{
+    
+    $q = 'SELECT * FROM photos ORDER BY upload_date DESC'; //get newest pic first
+    $r = $pdo->query($q);
+    
+    if($r){//if query executed successfully
+        
+        $r->setFetchMode(PDO::FETCH_CLASS, 'Photos'); //set fetch mode
+        
+        //retrieve page
+        $page_title = 'Gallery';
+        include('includes/header.inc.php');
+        include('views/gallery.html');
+        include('includes/footer.inc.php');
+    
+    }else{
+        throw new Exception('Sorry, something went wrong, please try again.');
+    }
+    
+    
+}catch(Exception $e){
+    $page_title = "Errors";
+    include('includes/header.inc.php');
+    include('views/errors.html');
+    include('includes/footer.inc.php');
+}
 
 
 ?>
