ac5f739e044d6258477da66e6c3d367525de8408
Rezaa91 <rezaa91@hotmail.co.uk>
2018-05-11 14:30:58 +0100

    fixed bugs

--- account.php
+++ account.php
@@ -2,12 +2,14 @@
 
 require('core/init.php');
 
-if(isset($user)){
+if(isset($user) && ($_SESSION['agent'] == md5($_SERVER['HTTP_USER_AGENT'])) ){
+    
     $page_title = $user->getUsername();
     include('includes/header.inc.php');
     include('views/account.html');
     include('includes/footer.inc.php');
-}else{
+    
+}else{ //if page accessed in error - redirect to homepage
     header('location:index.php');
     exit();
 }
