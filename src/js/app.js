9a688ce49deb6595ae1eb361e85c2ee50b4e4fbb
Rezaa91 <rezaa91@hotmail.co.uk>
2018-05-12 22:32:25 +0100

    added functionality for navigation - to be able to open and close menu on btn press

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function() {
+
+    let sidebar = document.getElementById('sidebar_wrapper');
+    let sidebar_left = document.getElementById('sidebar_left'); //left hand side of navigation column
+    let sidebar_right = document.getElementById('sidebar_right'); //right hand side of navigation column
+    
+    let content_wrapper = document.getElementById('content_wrapper'); //rest of page
+    let hide_menu_btn = document.getElementById('hide_menu'); //button which will be used to toggle menu when clicked
+    
+    
+    //toggle menu when btn pressed
+    hide_menu_btn.addEventListener('click', function() {
+        if(sidebar_right.style.display !== 'none'){
+            sidebar_right.style.display = 'none';
+        }else{
+            sidebar_right.style.display = 'block';
+        }
+    })
+    
+    //hide menu when content hovered over
+    content_wrapper.addEventListener('mouseover',function(e){
+        sidebar_right.style.display = 'none';
+    })
+    
+})
