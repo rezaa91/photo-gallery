5ad4a1e0cf98c818c21c9c8314c0f6d7a211f2ca
rezaa91 <rezaa91@hotmail.co.uk>
2018-05-20 19:40:04 +0100

    added config array, ammended class dir to accomodate www folder and listed server settings for local and live server

--- core/init.php
+++ core/init.php
@@ -2,7 +2,7 @@
 
 //load classes automatically when used
 function class_loader($class){
-    require('classes/' . $class . '.php');
+    require('../classes/' . $class . '.php');
 }
 
 spl_autoload_register('class_loader');
@@ -17,10 +17,37 @@ if(isset($_SESSION['user']) && (isset($_SESSION['agent']) && $_SESSION['agent'] 
 }
 
 
+//config file to store database data
+$_CONFIG = [
+    'server' => [
+        'host' => 'db738551371.db.1and1.com',
+        'port' => 3306,
+        'username' => 'dbo738551371',
+        'password' => 'Issaee50!',
+        'db_name' => 'db738551371'
+    ],
+    'local_server' => [
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'Issaee50!',
+        'db_name' => 'gallery'
+    ]
+];
+
+
+//comment the below code out on localhost
+/*
+error_reporting(0);
+ini_set('display_errors',0);
+*/
+
+
+
+
 //connect to database
 try{
     
-    $pdo = new PDO('mysql:dbname=gallery; host=127.0.0.1', 'root','Issaee50!');
+    $pdo = new PDO('mysql:host=' . $_CONFIG['local_server']['host'] . /*';port=' . $_CONFIG['server']['port'] .*/ ';dbname=' . $_CONFIG['local_server']['db_name'], $_CONFIG['local_server']['username'], $_CONFIG['local_server']['password']);
     
 }catch(PDOException $e){ //if error connecting to database, inform user
     $page_title = 'Error!';
