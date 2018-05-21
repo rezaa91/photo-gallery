dd3f49a8542fdc34ba4006f9fc3b252c7f6ab44d
rezaa91 <rezaa91@hotmail.co.uk>
2018-05-21 22:09:15 +0100

    removed all content other than social media links

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,18 +1,9 @@
-                <footer class="top-spacing">
-                    <div id="left-footer">
-                        <!--description-->
-                        <h3 class='lead primary'>AX Photography</h3>
-                        <p>AX Photography is a website application portfolio which allows pictures to be uploaded anywhere in the world for all to see.</p>
-                    </div><!--end of left footer-->
-                    
-                    <div id="right-footer">
-                        <!--social media links-->
-                        <ul>
-                            <li><a href="mailto:hissaee@yahoo.co.uk" title="email ax photography" class="sub-header secondary"><i class="fas fa-envelope-open"></i></a></li>
-                            <li><a href="https://www.flickr.com/photos/68234194@N05/" title="view flikr account" target="_blank" class="sub-header secondary"><i class="fab fa-flickr"></i></a></li>
-                        </ul>
-                        
-                    </div><!--end of right footer-->
+                <footer>
+                    <!--social media links-->
+                    <ul>
+                        <li><a href="mailto:hissaee@yahoo.co.uk" title="email ax photography" class="secondary"><i class="fas fa-envelope-open"></i></a></li>
+                        <li><a href="https://www.flickr.com/photos/68234194@N05/" title="view flikr account" target="_blank" class="secondary"><i class="fab fa-flickr"></i></a></li>
+                    </ul>    
                     
                 </footer><!--end of footer-->
                 
